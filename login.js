d09f668ced1b710dad14ced87f2d2ba6dc4d3701
Samuel <samucacsg@gmail.com>
2023-07-05 12:04:08 -0300

    arrumou os códigos

--- login.js
+++ login.js
@@ -1,33 +1,11 @@
-<<<<<<< HEAD
-/*
-Codigo de validação de formulario de login
-*/
 function Validar(){
     let email = document.getElementById('loginEmail').value;
     let senha = document.getElementById('loginSenha').value;
 
     if(!email || !senha){
-        alert("Campos de preenchimento obrigatório. Favor preencher");
+        alert("Favor preencha os campos para acessar o site");
     }
     else{
-        alert("Campos preenchidos com sucesso!")
+        window.location.href = "aula2.html";
     }
 }
-
-=======
-/*
-Codigo de validação de formulario de login
-*/
-function Validar(){
-    let email = document.getElementById('loginEmail').value;
-    let senha = document.getElementById('loginSenha').value;
-
-    if(!email || !senha){
-        alert("Campos de preenchimento obrigatório. Favor preencher");
-    }
-    else{
-        alert("Campos preenchidos com sucesso!")
-    }
-}
-
->>>>>>> 670c40c784f7610a09ed601912766baa16e53422
