a7479ddca7a0b4959d8bf945e28f121db488969a
Samuel <samucacsg@gmail.com>
2023-07-26 12:08:17 -0300

    arruma as funcoes excluir e editar

--- login.js
+++ login.js
@@ -34,7 +34,7 @@ function criaLista(){
 
     // laço de repetição para colocar nomes na tabela
     for(let i=0;i <= (dadosLista.length-1);i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger' onclick=''>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         
         document.getElementById("tabela").innerHTML = tabela;
     } 
